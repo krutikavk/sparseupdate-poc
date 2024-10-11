519ee2beabd0c0e89525c290ae85515eba6bab21
kkatelia <krutika_katelia@intuit.com>
2024-10-11 14:43:25 -0700

    adding spoon maven plugin

--- app/src/main/java/com/intuit/sparseupdate/ShowDataFetcher.java
+++ app/src/main/java/com/intuit/sparseupdate/ShowDataFetcher.java
@@ -3,19 +3,14 @@ package com.intuit.sparseupdate;
 import com.intuit.sparseupdate.generated.DgsConstants;
 import com.intuit.sparseupdate.generated.types.Show;
 import com.intuit.sparseupdate.generated.types.UpdateShowInput;
-import com.intuit.sparseupdate.generated.types.UpdateShowInputInvocationHandler;
-import com.intuit.sparseupdate.generated.types.IUpdateShowInput;
+import com.intuit.sparseupdate.generated.DgsConstants;
 import com.netflix.graphql.dgs.*;
 import com.netflix.graphql.dgs.exceptions.DgsBadRequestException;
-import com.netflix.graphql.dgs.exceptions.DgsEntityNotFoundException;
-import graphql.schema.DataFetchingEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
-import java.lang.reflect.UndeclaredThrowableException;
 import java.util.*;
 
 @DgsComponent
@@ -54,47 +49,8 @@ public class ShowDataFetcher {
         }
         Show show = data.get(input.getId());
 
-        /*
-            mutation updateShow {
-              updateShow(id: "one", releaseYear: 1985) {
-                id
-                title
-                releaseYear
-              }
-            }
-             No variables supplied.
-
-             ==> Value of rawVariables is undefined/not extracted for inline variables
-                 or client input values in variable other than set key "input" in RawVariableInstrumentation class
-
-             Better way is to get raw variable map as on line 75.
-         */
-//        Map<String, Object> rawVariables = dfe.getGraphQlContext().get("rawVariables");
-
-        Map<String,Object> rawVariables = dfe.getExecutionStepInfo().getArgument(
-                DgsConstants.MUTATION.UPDATESHOW_INPUT_ARGUMENT.Input);
-
-        UpdateShowInputInvocationHandler handler = new UpdateShowInputInvocationHandler(input);
-        IUpdateShowInput proxyObject = (IUpdateShowInput) Proxy.newProxyInstance(
-                IUpdateShowInput.class.getClassLoader(),
-                new Class<?>[] { IUpdateShowInput.class },
-                handler
-        );
-
-        //proxyObject invocation handler
-        if(rawVariables.containsKey("title")) {
-            proxyObject.setIsTitleSet();
-        }
-
-        //proxyObject invocation handler
-        if(rawVariables.containsKey("releaseYear")) {
-            proxyObject.setIsReleaseYearSet();
-        }
-
-//        System.out.println("proxyObject title presence field: " + proxyObject.isTitleSet() + " releaseYear presence field: " + proxyObject.isReleaseYearSet());
-//
-//        System.out.println("targetObject.getTitle(): " + proxyObject.getTitle());
-//        System.out.println("targetObject.getReleaseYear(): " + proxyObject.getTitle());
+        show.setTitle(input.getTitle());
+        show.setReleaseYear(input.getReleaseYear());
 
         return show;
     }
